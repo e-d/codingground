5e06d0fdf4eabdccff051d2ccfb1e81bb397ddb3
e-d <ed7181086@hotmail.com>
2016-09-16 09:20:45 -0600

    Created by Coding Ground Client

---
+++ New Project-20160916/ChairFinder.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class ChairFinder {
+
+     public static void main(String []args) {
+         LinkedList<Chair> chairs = new LinkedList();
+         for (int i = 0; i < 10; i++) {
+            Chair c = new Chair();
+            c.setComfortLevel(i);
+            chairs.add(c);
+         }
+         System.out.println("Most comfortable: " + findMostComfortableChair(chairs));
+     }
+     
+     private static Chair findMostComfortableChair(List<Chair> chairs) {
+         return null;
+     }
+}
